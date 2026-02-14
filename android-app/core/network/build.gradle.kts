5e4f1bb62eafcfce50ccf55cec716083cec73f18
David Batista <xmedavid@users.noreply.github.com>
2026-02-14 22:28:19 +0100

    fix: network bug with test

--- android-app/core/network/build.gradle.kts
+++ android-app/core/network/build.gradle.kts
@@ -33,4 +33,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    testImplementation("junit:junit:4.13.2")
 }
